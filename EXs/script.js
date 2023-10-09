d0a3e67d58e44d8e90a4dd9d60076193ff9ecded
l3b3al <alabdulaal.alii@gmail.com>
2023-10-09 09:38:44 +0300

    JS 2

---
+++ EXs/script.js
@@ -0,0 +1,31 @@
+let skills = [
+    {
+        "icon" : "assets/git.png",
+        "header" : "Git"
+    },
+    {
+        "icon" : "assets/figma.png",
+        "header" : "Figma"
+    },
+    {
+        "icon" : "assets/java.png",
+        "header" : "Java"
+    }
+]
+
+function fillSkills(){
+    document.getElementById("skillIconsCol").innerHTML = ""
+    for(let skill of skills){
+        let content = `
+            <div class="skillCard">
+                <div>
+                    <img class="skillIcon" src="${skill.icon}" alt="">
+                </div>
+                <div><p>${skill.header}</p></div>
+            </div>
+        `
+        document.getElementById("skillIconsCol").innerHTML += content
+    }
+}
+
+fillSkills()
